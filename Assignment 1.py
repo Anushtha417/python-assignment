35aea28138d8ddddb71f73642def25dc4b420863
vaishnaviambhorkar <vaishnaviambhorkar@gmail.com>
2025-10-08 18:36:51 +0530

    Upload assigment files

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,6 +1,8 @@
 a = 5
 b = 10
-print(a+b)
-print(a-b)
-print(a*b)
-print(a/b)
+print("Enter the first number = 5") 
+print("Enter the second number = 10")
+print("Addition =", a + b)
+print("Subtraction =", a - b) #a = 5, b=10
+print("Multiplication =", a * b)
+print("Division = ", a / b)
